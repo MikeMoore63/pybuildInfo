6cb6a7f1d3b7c394d7ae89faf7ab4eda504f60c9
Mike Moore <z_z_zebra@yahoo.com>
2022-08-05 09:19:44 +0100

    fix(setup.py): reformat code and remove no longer needed old build system in favour of setuptools-golang

--- setup.py
+++ setup.py
@@ -1,22 +1,10 @@
 """Setup for checksig package"""
-from distutils.errors import CompileError
-from subprocess import call
 from io import open
+
 from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
-
-class build_go_ext(build_ext):
-    """Custom command to build extension from Go source files"""
-    def build_extension(self, ext):
-        ext_path = self.get_ext_fullpath(ext.name)
-        cmd = ['go', 'build', '-buildmode=c-shared', '-o', ext_path]
-        cmd += ext.sources
-        out = call(cmd)
-        if out != 0:
-            raise CompileError('Go build failed')
 
 setup(
     name='pyGoBuildInfo',
@@ -31,9 +19,5 @@ setup(
     build_golang={'root': 'github.com/MikeMoore63/pybuildInfo'},
     ext_modules=[Extension('_pyGoBuildinfo', ['pyGoBuildinfo.go'])],
     setup_requires=['setuptools-golang'],
-    #ext_modules=[
-    #    Extension('_pyGoBuildinfo', ['pyGoBuildinfo.go','export.go'])
-    #],
-    #cmdclass={'build_ext': build_go_ext},
     zip_safe=False,
 )

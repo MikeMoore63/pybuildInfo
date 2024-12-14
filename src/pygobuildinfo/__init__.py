f83619a8fcb209bc81ab4390f51fa567861fdc49
Mike Moore <mike.moore@hsbc.com>
2024-12-14 09:35:39 +0000

    feat: add indirect flag when parsing go.mod bump go minor version, improve test prog for local testing, reformat for flake8

--- src/pygobuildinfo/__init__.py
+++ src/pygobuildinfo/__init__.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import
 
 from pygobuildinfo._pyinstaller import get_hook_dirs, get_PyInstaller_tests
-__all__ = [
-    "get_hook_dirs",
-    "get_PyInstaller_tests",
-    "get_go_build_info"
-]
+
+__all__ = ["get_hook_dirs", "get_PyInstaller_tests", "get_go_build_info"]
 
 """Extract buildinfo from go built binaries"""
 
@@ -20,8 +17,8 @@ from pathlib import Path
 
 # Location of shared library
 here = Path(__file__).absolute().parent
-ext_suffix = get_config_var('EXT_SUFFIX')
-so_file = os.path.join(here, ('_pyGoBuildinfo' + ext_suffix))
+ext_suffix = get_config_var("EXT_SUFFIX")
+so_file = os.path.join(here, ("_pyGoBuildinfo" + ext_suffix))
 
 # Load functions from shared library set their signatures
 so = ctypes.cdll.LoadLibrary(so_file)
@@ -39,42 +36,51 @@ def get_go_build_info(file_name):
     """Check (in parallel) digital signature of all files in root_dir.
     We assume there's a sha1sum.txt file under root_dir
     """
-    res = get_go_build_info_so(file_name.encode('utf-8'))
+    res = get_go_build_info_so(file_name.encode("utf-8"))
     if res is not None:
         result = {"error": "Error converting result to json"}
         try:
-            result = json.loads(ctypes.string_at(res).decode('utf-8'))
+            result = json.loads(ctypes.string_at(res).decode("utf-8"))
         except json.JSONDecodeError as e:
             pass
         finally:
             free(res)
-        return (result)
+        return result
+
 
 def get_go_mod(file_name):
     """Check (in parallel) digital signature of all files in root_dir.
     We assume there's a sha1sum.txt file under root_dir
     """
-    res = get_go_mod_so(file_name.encode('utf-8'))
+    res = get_go_mod_so(file_name.encode("utf-8"))
     if res is not None:
         result = {"error": "Error converting result to json"}
         try:
-            temp_result = json.loads(ctypes.string_at(res).decode('utf-8'))
+            temp_result = json.loads(ctypes.string_at(res).decode("utf-8"))
             if "error" not in temp_result:
-                actual_result = {"Deps":[]}
-                if "Module" in temp_result and "Mod" in temp_result["Module"] and "Path" in temp_result["Module"]["Mod"]:
+                actual_result = {"Deps": []}
+                if (
+                    "Module" in temp_result
+                    and "Mod" in temp_result["Module"]
+                    and "Path" in temp_result["Module"]["Mod"]
+                ):
                     actual_result["Path"] = temp_result["Module"]["Mod"]["Path"]
                 if "Go" in temp_result and "Version" in temp_result["Go"]:
                     actual_result["GoVersion"] = f"go{temp_result['Go']['Version']}"
-                for module_required in temp_result.get("Require",[]):
+                for module_required in temp_result.get("Require", []):
                     if "Mod" not in module_required:
                         continue
                     if "Path" not in module_required["Mod"]:
                         continue
                     if "Version" not in module_required["Mod"]:
                         continue
-                    actual_result["Deps"].append({
-                        "Path": module_required["Mod"]["Path"],
-                        "Version": module_required["Mod"]["Version"]})
+                    actual_result["Deps"].append(
+                        {
+                            "Path": module_required["Mod"]["Path"],
+                            "Version": module_required["Mod"]["Version"],
+                            "Indirect": module_required.get("Indirect", False),
+                        }
+                    )
                 result = actual_result
             else:
                 result = temp_result
@@ -82,12 +88,12 @@ def get_go_mod(file_name):
             pass
         finally:
             free(res)
-        return (result)
+        return result
 
 
 def get_go_sum(file_name):
     result = {"error": "not a valid go sum file"}
-    with open(file_name,mode="rt", encoding="utf-8") as gsf:
+    with open(file_name, mode="rt", encoding="utf-8") as gsf:
         data = {}
         for line in gsf.readlines():
             sumfields = line.strip().split()
@@ -96,7 +102,12 @@ def get_go_sum(file_name):
             if sumfields[1].endswith("/go.mod"):
                 sumfields[1] = sumfields[1][:-7]
             data[sumfields[0]] = {"Version": sumfields[1], "Sum": sumfields[2]}
-        data = {"Deps": [{"Path": k, "Version": data[k]["Version"], "Sum": data[k]["Sum"]} for k in data]}
+        data = {
+            "Deps": [
+                {"Path": k, "Version": data[k]["Version"], "Sum": data[k]["Sum"]}
+                for k in data
+            ]
+        }
     if data:
         return data
     return result
--- src/pygobuildinfo/test_pyGoBuildInfo.py
+++ src/pygobuildinfo/test_pyGoBuildInfo.py
@@ -1,22 +1,35 @@
 from pygobuildinfo import get_go_build_info, get_go_mod, get_go_sum
 import glob
 import sys
+import os
 import json
 import concurrent.futures
 
 
 def test_get_info(file):
+    if not os.path.isfile(file):
+        print(f"{file} does not exist skipping")
+        return
+
     res = get_go_build_info(file)
-    print(json.dumps(res,indent=4))
+    print(f"{file}\n{json.dumps(res,indent=4)}")
 
 
 def test_go_mod(file):
+    if not os.path.isfile(file):
+        print(f"{file} does not exist skipping")
+        return
     res = get_go_mod(file)
-    print(json.dumps(res,indent=4))
+    print(f"{file}\n{json.dumps(res,indent=4)}")
 
 def test_go_sum(file):
+    if not os.path.isfile(file):
+        print(f"{file} does not exist skipping")
+        return
     res = get_go_sum(file)
-    print(json.dumps(res,indent=4))
+    print(f"{file}\n{json.dumps(res,indent=4)}")
+
+
 
 
 test_go_mod("/Users/mike/go/src/pygobuildInfo/go.mod")
@@ -37,8 +50,16 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
     for arg  in sys.argv[1:]:
         for file in glob.glob(arg,recursive=True):
             futures.append(executor.submit(get_go_build_info,file))
+            if file.endswith(".sum"):
+                futures.append(executor.submit(get_go_sum,file))
+            if file.endswith(".mod"):
+                futures.append(executor.submit(get_go_mod,file))
 
     for future in concurrent.futures.as_completed(futures):
-        result = future.result()
-        print(json.dumps(result,indent=4))
+        try:
+            result = future.result()
+            print(json.dumps(result,indent=4))
+        except Exception as e:
+            print(e)
+
     print(f"processed {len(futures)} files")

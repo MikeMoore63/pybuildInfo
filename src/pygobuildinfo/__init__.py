8062ff6a1595a6842627215884dfe0de868e9151
Mike Moore <z_z_zebra@yahoo.com>
2026-07-02 13:45:42 +0100

    feat: remove hook imports and export as have been deleted as not required

--- src/pygobuildinfo/__init__.py
+++ src/pygobuildinfo/__init__.py
@@ -1,11 +1,7 @@
 from __future__ import absolute_import
-
-from pygobuildinfo._pyinstaller import get_hook_dirs, get_PyInstaller_tests
 from pygorpmrustinfo import get_go_build_info, get_go_mod, get_go_sum
 
 __all__ = [
-    "get_hook_dirs",
-    "get_PyInstaller_tests",
     "get_go_build_info",
     "get_go_mod",
     "get_go_sum",
